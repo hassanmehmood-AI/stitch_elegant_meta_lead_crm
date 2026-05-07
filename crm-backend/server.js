b88e682fed783a7b2d5921460b415e440a5969a1
Hassan Mehmood <hassanmehmood@example.com>
2026-05-07 16:40:42 -0400

    Allow all CORS origins for production

--- crm-backend/server.js
+++ crm-backend/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.use("/api/auth",     require("./routes/auth"));
